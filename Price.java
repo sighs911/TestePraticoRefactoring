e7da66808d70e2bffd449027915e8ee96751903a
sighs911 <iskuroda@minha.fag.edu.br>
2024-11-26 23:19:46 +0000

    Commit 14

--- Price.java
+++ Price.java
@@ -2,4 +2,14 @@ public abstract class Price {
   public abstract int getPriceCode();
 
   public abstract double getCharge(int daysRented);
+
+  public int getFrequentRenterPoints(int daysRented, Rental aRental) {
+    int frequentRenterPoints = 0;
+    // add frequent renter points
+    frequentRenterPoints++;
+    if ((aRental.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+        aRental.getDaysRented() > 1)
+      frequentRenterPoints++;
+    return frequentRenterPoints;
+  }
 }
