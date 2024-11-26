cc7a6302876ce8cf9e2fd875dd63170d5cccf234
sighs911 <iskuroda@minha.fag.edu.br>
2024-11-26 22:56:45 +0000

    Commit 9

--- Movie.java
+++ Movie.java
@@ -23,4 +23,24 @@ public class Movie {
   public String getTitle (){
       return _title;
   }
+  public double getCharge(int daysRented, Rental aRental){
+      // determine amounts for each line
+        double thisAmount = 0;
+        switch (aRental.getMovie().getPriceCode()) {
+           case Movie.REGULAR:
+              thisAmount += 2;
+              if (aRental.getDaysRented() > 2)
+                 thisAmount += (aRental.getDaysRented() - 2) * 1.5;
+              break;
+           case Movie.NEW_RELEASE:
+              thisAmount += aRental.getDaysRented() * 3;
+              break;
+           case Movie.CHILDRENS:
+              thisAmount += 1.5;
+              if (aRental.getDaysRented() > 3)
+                 thisAmount += (aRental.getDaysRented() - 3) * 1.5;
+              break;
+        }
+        return thisAmount;
+  }
 }
