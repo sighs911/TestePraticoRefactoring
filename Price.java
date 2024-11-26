b0d98d15d083a0567b1005274cccdfe64155b3bd
sighs911 <iskuroda@minha.fag.edu.br>
2024-11-26 23:15:37 +0000

    Commit 13

--- Price.java
+++ Price.java
@@ -1,26 +1,5 @@
 public abstract class Price {
   public abstract int getPriceCode();
 
-  public double getCharge(int daysRented, Rental aRental) {
-    double thisAmount = 0;
-
-    switch (aRental.getMovie().getPriceCode()) {
-      case Movie.REGULAR:
-        thisAmount += 2;
-        if (aRental.getDaysRented() > 2) {
-          thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-        }
-        break;
-      case Movie.NEW_RELEASE:
-        thisAmount += aRental.getDaysRented() * 3;
-        break;
-      case Movie.CHILDRENS:
-        thisAmount += 1.5;
-        if (aRental.getDaysRented() > 3) {
-          thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-        }
-        break;
-    }
-    return thisAmount;
-  }
+  public abstract double getCharge(int daysRented);
 }
