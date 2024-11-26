2aa1c02d67bf5d34ab93a1363ec7461ce40e43ec
sighs911 <iskuroda@minha.fag.edu.br>
2024-11-26 23:48:13 +0000

    Commit 18

--- Statement.java
+++ Statement.java
@@ -1,3 +1,18 @@
-public class Statement{
-
+public abstract class Statement {
+  public String value(Customer aCustomer) {
+    Enumeration rentals = aCustomer.getRentals();
+    String result = generateHeader(aCustomer);
+    while (rentals.hasMoreElements()) {
+      Rental each = (Rental) rentals.nextElement();
+      result += formatRentalDetails(generateRentalDetails(each));
+    }
+    result += generateFooter(aCustomer);
+    return result;
+  }
+  protected String generateRentalDetails(Rental each) {
+    return each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge());
+  }
+  protected abstract String generateHeader(Customer aCustomer);
+  protected abstract String generateFooter(Customer aCustomer);
+  protected abstract String formatRentalDetails(String details);
 }
